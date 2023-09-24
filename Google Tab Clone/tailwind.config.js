e71b70ca4aa9870ff4bf3838a7204c13fa2d6e62
Akshat <akshat.02jan@gmail.com>
2023-09-24 19:59:23 +0530

    Adding new project

---
+++ Google Tab Clone/tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {
+      boxShadow:
+      {
+        'custom-box-shadow': '0px 0px 13px -7px rgba(0,0,0,0.96)'
+      }
+    },
+  },
+  plugins: [],
+}
+
